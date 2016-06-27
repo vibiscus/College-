e67ed72231c01950876796130f49b369d99fecdc
vibiscus <virginie.rieber@gmail.com>
2016-06-27 07:54:28 +0200

    Create note.java

---
+++ note.java
@@ -0,0 +1,12 @@
+public class note {
+	float valeurNote;
+	eleve eleve; 
+	matiere matiere;
+	
+	// constructeur
+		public note(float valeurNote, eleve eleve, matiere matiere){
+			this.valeurNote=valeurNote;
+			this.eleve=eleve;
+			this.matiere = matiere;
+		}
+}
